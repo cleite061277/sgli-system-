a49581dae9fc88c9f880f84dcee3c7eeec3c7d39
Cicero Leite <cleite061277@gmail.com>
2025-11-10 23:34:30 -0300

    Fix: Desabilitar Celery para preview deploy

--- sgli_project/__init__.py
+++ sgli_project/__init__.py
@@ -1,6 +1,6 @@
 """
 Garante que o Celery é carregado quando Django inicia
 """
-from .celery import app as celery_app
+#from .celery import app as celery_app
 
-__all__ = ('celery_app',)
+#__all__ = ('celery_app',)
